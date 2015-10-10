5d16b11ccc3b77f19d2e1f9a7a41898ea6e007a3
dwidista <dnsdista@gmail.com>
2015-10-10 09:15:46 +0700

    Pengumpulan Tugas 2
    Nama : Dwi dista PU
    Kelas : TI-2E
    NIm : 1441180065

---
+++ tugas-2015-02-dwidista-master/tugas-2015-02/Form1.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextHasil = New System.Windows.Forms.TextBox()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.tambah = New System.Windows.Forms.Button()
+        Me.samadengan = New System.Windows.Forms.Button()
+        Me.hapus = New System.Windows.Forms.Button()
+        Me.kurang = New System.Windows.Forms.Button()
+        Me.kali = New System.Windows.Forms.Button()
+        Me.bagi = New System.Windows.Forms.Button()
+        Me.sembilan = New System.Windows.Forms.Button()
+        Me.delapan = New System.Windows.Forms.Button()
+        Me.tujuh = New System.Windows.Forms.Button()
+        Me.empat = New System.Windows.Forms.Button()
+        Me.lima = New System.Windows.Forms.Button()
+        Me.enam = New System.Windows.Forms.Button()
+        Me.satu = New System.Windows.Forms.Button()
+        Me.dua = New System.Windows.Forms.Button()
+        Me.tiga = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextHasil
+        '
+        Me.TextHasil.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextHasil.Location = New System.Drawing.Point(13, 13)
+        Me.TextHasil.Multiline = True
+        Me.TextHasil.Name = "TextHasil"
+        Me.TextHasil.ReadOnly = True
+        Me.TextHasil.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextHasil.Size = New System.Drawing.Size(273, 44)
+        Me.TextHasil.TabIndex = 0
+        Me.TextHasil.Text = "0"
+        Me.TextHasil.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.Location = New System.Drawing.Point(13, 214)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(53, 53)
+        Me.Button0.TabIndex = 1
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'tambah
+        '
+        Me.tambah.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tambah.Location = New System.Drawing.Point(182, 212)
+        Me.tambah.Name = "tambah"
+        Me.tambah.Size = New System.Drawing.Size(49, 53)
+        Me.tambah.TabIndex = 2
+        Me.tambah.Text = "+"
+        Me.tambah.UseVisualStyleBackColor = True
+        '
+        'samadengan
+        '
+        Me.samadengan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.samadengan.Location = New System.Drawing.Point(72, 214)
+        Me.samadengan.Name = "samadengan"
+        Me.samadengan.Size = New System.Drawing.Size(104, 53)
+        Me.samadengan.TabIndex = 3
+        Me.samadengan.Text = "="
+        Me.samadengan.UseVisualStyleBackColor = True
+        '
+        'hapus
+        '
+        Me.hapus.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hapus.Location = New System.Drawing.Point(237, 63)
+        Me.hapus.Name = "hapus"
+        Me.hapus.Size = New System.Drawing.Size(49, 202)
+        Me.hapus.TabIndex = 4
+        Me.hapus.Text = "Clear"
+        Me.hapus.UseVisualStyleBackColor = True
+        '
+        'kurang
+        '
+        Me.kurang.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kurang.Location = New System.Drawing.Point(182, 159)
+        Me.kurang.Name = "kurang"
+        Me.kurang.Size = New System.Drawing.Size(49, 47)
+        Me.kurang.TabIndex = 5
+        Me.kurang.Text = "-"
+        Me.kurang.UseVisualStyleBackColor = True
+        '
+        'kali
+        '
+        Me.kali.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kali.Location = New System.Drawing.Point(182, 112)
+        Me.kali.Name = "kali"
+        Me.kali.Size = New System.Drawing.Size(49, 41)
+        Me.kali.TabIndex = 6
+        Me.kali.Text = "*"
+        Me.kali.UseVisualStyleBackColor = True
+        '
+        'bagi
+        '
+        Me.bagi.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bagi.Location = New System.Drawing.Point(182, 63)
+        Me.bagi.Name = "bagi"
+        Me.bagi.Size = New System.Drawing.Size(49, 42)
+        Me.bagi.TabIndex = 7
+        Me.bagi.Text = "/"
+        Me.bagi.UseVisualStyleBackColor = True
+        '
+        'sembilan
+        '
+        Me.sembilan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sembilan.Location = New System.Drawing.Point(127, 63)
+        Me.sembilan.Name = "sembilan"
+        Me.sembilan.Size = New System.Drawing.Size(49, 42)
+        Me.sembilan.TabIndex = 8
+        Me.sembilan.Text = "9"
+        Me.sembilan.UseVisualStyleBackColor = True
+        '
+        'delapan
+        '
+        Me.delapan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.delapan.Location = New System.Drawing.Point(72, 63)
+        Me.delapan.Name = "delapan"
+        Me.delapan.Size = New System.Drawing.Size(49, 42)
+        Me.delapan.TabIndex = 9
+        Me.delapan.Text = "8"
+        Me.delapan.UseVisualStyleBackColor = True
+        '
+        'tujuh
+        '
+        Me.tujuh.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tujuh.Location = New System.Drawing.Point(12, 63)
+        Me.tujuh.Name = "tujuh"
+        Me.tujuh.Size = New System.Drawing.Size(49, 42)
+        Me.tujuh.TabIndex = 10
+        Me.tujuh.Text = "7"
+        Me.tujuh.UseVisualStyleBackColor = True
+        '
+        'empat
+        '
+        Me.empat.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.empat.Location = New System.Drawing.Point(12, 111)
+        Me.empat.Name = "empat"
+        Me.empat.Size = New System.Drawing.Size(49, 42)
+        Me.empat.TabIndex = 11
+        Me.empat.Text = "4"
+        Me.empat.UseVisualStyleBackColor = True
+        '
+        'lima
+        '
+        Me.lima.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lima.Location = New System.Drawing.Point(72, 111)
+        Me.lima.Name = "lima"
+        Me.lima.Size = New System.Drawing.Size(49, 42)
+        Me.lima.TabIndex = 12
+        Me.lima.Text = "5"
+        Me.lima.UseVisualStyleBackColor = True
+        '
+        'enam
+        '
+        Me.enam.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.enam.Location = New System.Drawing.Point(127, 111)
+        Me.enam.Name = "enam"
+        Me.enam.Size = New System.Drawing.Size(49, 42)
+        Me.enam.TabIndex = 13
+        Me.enam.Text = "6"
+        Me.enam.UseVisualStyleBackColor = True
+        '
+        'satu
+        '
+        Me.satu.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.satu.Location = New System.Drawing.Point(13, 158)
+        Me.satu.Name = "satu"
+        Me.satu.Size = New System.Drawing.Size(53, 48)
+        Me.satu.TabIndex = 14
+        Me.satu.Text = "1"
+        Me.satu.UseVisualStyleBackColor = True
+        '
+        'dua
+        '
+        Me.dua.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dua.Location = New System.Drawing.Point(72, 160)
+        Me.dua.Name = "dua"
+        Me.dua.Size = New System.Drawing.Size(49, 46)
+        Me.dua.TabIndex = 15
+        Me.dua.Text = "2"
+        Me.dua.UseVisualStyleBackColor = True
+        '
+        'tiga
+        '
+        Me.tiga.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tiga.Location = New System.Drawing.Point(127, 159)
+        Me.tiga.Name = "tiga"
+        Me.tiga.Size = New System.Drawing.Size(49, 47)
+        Me.tiga.TabIndex = 16
+        Me.tiga.Text = "3"
+        Me.tiga.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(293, 281)
+        Me.Controls.Add(Me.tiga)
+        Me.Controls.Add(Me.dua)
+        Me.Controls.Add(Me.satu)
+        Me.Controls.Add(Me.enam)
+        Me.Controls.Add(Me.lima)
+        Me.Controls.Add(Me.empat)
+        Me.Controls.Add(Me.tujuh)
+        Me.Controls.Add(Me.delapan)
+        Me.Controls.Add(Me.sembilan)
+        Me.Controls.Add(Me.bagi)
+        Me.Controls.Add(Me.kali)
+        Me.Controls.Add(Me.kurang)
+        Me.Controls.Add(Me.hapus)
+        Me.Controls.Add(Me.samadengan)
+        Me.Controls.Add(Me.tambah)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.TextHasil)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "KalkulatorDwiDista"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextHasil As System.Windows.Forms.TextBox
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents tambah As System.Windows.Forms.Button
+    Friend WithEvents samadengan As System.Windows.Forms.Button
+    Friend WithEvents hapus As System.Windows.Forms.Button
+    Friend WithEvents kurang As System.Windows.Forms.Button
+    Friend WithEvents kali As System.Windows.Forms.Button
+    Friend WithEvents bagi As System.Windows.Forms.Button
+    Friend WithEvents sembilan As System.Windows.Forms.Button
+    Friend WithEvents delapan As System.Windows.Forms.Button
+    Friend WithEvents tujuh As System.Windows.Forms.Button
+    Friend WithEvents empat As System.Windows.Forms.Button
+    Friend WithEvents lima As System.Windows.Forms.Button
+    Friend WithEvents enam As System.Windows.Forms.Button
+    Friend WithEvents satu As System.Windows.Forms.Button
+    Friend WithEvents dua As System.Windows.Forms.Button
+    Friend WithEvents tiga As System.Windows.Forms.Button
+
+End Class
---
+++ tugas-2015-02-dwidista-master/tugas-2015-02/Form1.vb
@@ -0,0 +1,152 @@
+﻿Public Class Form1
+    Dim nilai1, nilai2 As Long
+    Dim hasil As Double
+    Dim operasi, txtnilai1, txtnilai2 As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles samadengan.Click
+        nilai1 = Val(txtnilai1)
+        nilai2 = Val(txtnilai2)
+        If operasi = "+" Then
+            hasil = nilai1 + nilai2
+            TextHasil.Text = hasil
+        ElseIf operasi = "-" Then
+            hasil = nilai1 - nilai2
+            TextHasil.Text = hasil
+        ElseIf operasi = "*" Then
+            hasil = nilai1 * nilai2
+            TextHasil.Text = hasil
+        ElseIf operasi = ":" Then
+            hasil = nilai1 / nilai2
+            TextHasil.Text = hasil
+        End If
+        operasi = ""
+        txtnilai1 = ""
+        txtnilai2 = ""
+        txtnilai1 = Str$(hasil)
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextHasil.TextChanged
+
+    End Sub
+
+    Private Sub satu_Click(sender As Object, e As EventArgs) Handles satu.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "1"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "1"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub dua_Click(sender As Object, e As EventArgs) Handles dua.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "2"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "2"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub tiga_Click(sender As Object, e As EventArgs) Handles tiga.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "3"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "3"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub empat_Click(sender As Object, e As EventArgs) Handles empat.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "4"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "4"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub lima_Click(sender As Object, e As EventArgs) Handles lima.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "5"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "5"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub enam_Click(sender As Object, e As EventArgs) Handles enam.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "6"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "6"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub tujuh_Click(sender As Object, e As EventArgs) Handles tujuh.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "7"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "7"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub delapan_Click(sender As Object, e As EventArgs) Handles delapan.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "8"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "8"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub sembilan_Click(sender As Object, e As EventArgs) Handles sembilan.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "9"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "9"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        If operasi = "" Then
+            txtnilai1 = txtnilai1 + "0"
+            TextHasil.Text = txtnilai1
+        Else : txtnilai2 = txtnilai2 + "0"
+            TextHasil.Text = txtnilai2
+        End If
+    End Sub
+
+    Private Sub tambah_Click(sender As Object, e As EventArgs) Handles tambah.Click
+        operasi = "+"
+    End Sub
+
+    Private Sub kurang_Click(sender As Object, e As EventArgs) Handles kurang.Click
+        operasi = "-"
+    End Sub
+
+    Private Sub kali_Click(sender As Object, e As EventArgs) Handles kali.Click
+        operasi = "*"
+    End Sub
+
+    Private Sub bagi_Click(sender As Object, e As EventArgs) Handles bagi.Click
+        operasi = ":"
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles hapus.Click
+        operasi = ""
+        txtnilai1 = ""
+        txtnilai2 = ""
+        TextHasil.Clear()
+    End Sub
+
+    Private Function nila1() As Long
+        Throw New NotImplementedException
+    End Function
+
+End Class
